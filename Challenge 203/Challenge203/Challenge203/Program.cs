a2e23ad94cf6a3844f098a6313e83741155ff983
Daniel <danlypemol@gmail.com>
2021-01-26 14:51:26 +0100

    Made a solution for Challenge 203

--- Challenge 203/Challenge203/Challenge203/Program.cs
+++ Challenge 203/Challenge203/Challenge203/Program.cs
@@ -10,6 +10,39 @@ namespace Challenge203
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Press any key to begin checking for prime numbers.");
+            PrimeChecker(Console.ReadKey());
+        }
+
+        static void PrimeChecker(System.ConsoleKeyInfo input)
+        {
+            int maximum = 97;
+            int minimum = 2;
+            int count = 0;
+
+            Console.WriteLine("\n Prime number status of the numbers 2 to 97:");
+
+            for (int i = minimum; i < maximum + 1; i++)
+            {
+                for (int j = 2; j < i; j++)
+                {
+                    if (i % j != 0)
+                    {
+                        count++;
+                    }
+                }
+                if (count == (i - 2))
+                {
+                    Console.WriteLine(i + ": Yes.");
+                }
+                else
+                {
+                    Console.WriteLine(i + ": No.");
+                }
+
+                count = 0;
+            }
+            Console.ReadKey();
         }
     }
 }
